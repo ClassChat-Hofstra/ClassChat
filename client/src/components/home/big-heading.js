f922988e4a0f250e0ab763672b51dd3a7a6d3015
Mike Cappeller <mikecappeller@gmail.com>
2021-05-13 20:55:12 -0400

    Change lorum ipsum on landing page

--- client/src/components/home/big-heading.js
+++ client/src/components/home/big-heading.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 
 function bigHeading() {
-    return (
-        <div>
-            <h1>ClassChat</h1>
-            <h5>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Adipiscing enim eu turpis egestas pretium aenean. Convallis a cras
-              semper auctor neque vitae. Elit eget gravida cum sociis.
-            </h5>
-          </div>
-    )
+  return (
+    <div>
+      <h1>ClassChat</h1>
+      <h5>
+        We understand that the COVID lockdown sucks. So we are providing you a
+        way to get in touch with the classmates that you may not get the chance
+        to meet in person. Just enter your schedule and you will automatically
+        be added to a chat with students who share the same course.{" "}
+        <em>It's that simple!</em>
+      </h5>
+    </div>
+  );
 }
 
 export default bigHeading;
